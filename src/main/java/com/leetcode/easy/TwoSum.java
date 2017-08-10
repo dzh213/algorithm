ad2d8abe18616a35ef6c18f925aa9999dffc39fa
donghao <dzh213@163.com>
2017-08-10 13:51:29 +0800

    构建版本库

---
+++ src/main/java/com/leetcode/easy/TwoSum.java
@@ -0,0 +1,30 @@
+package com.leetcode.easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by donghao on 2017/8/10.
+ */
+public class TwoSum {
+    public static void main(String[] agrs){
+        int[] numbers = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = twoSum(numbers,target);
+        System.out.println("aa");
+    }
+
+    public static int[] twoSum(int[] numbers, int target) {
+        int[] result = new int[2];
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < numbers.length; i++) {
+            if (map.containsKey(target - numbers[i])) {
+                result[1] = i + 1;
+                result[0] = map.get(target - numbers[i]);
+                return result;
+            }
+            map.put(numbers[i], i + 1);
+        }
+        return result;
+    }
+}
