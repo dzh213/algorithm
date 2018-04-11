f507cd5c8a43d30faf09c99a5130b79d3fd2fd84
donghao <dzh213@163.com>
2018-04-11 09:18:08 +0800

    阻塞和非阻塞实现生产者和消费者

---
+++ src/main/java/com/algorithm/offer/BlockingQueue.java
@@ -0,0 +1,53 @@
+package com.algorithm.offer;
+
+import java.util.concurrent.ArrayBlockingQueue;
+
+/**
+ * Created by donghao on 2018/4/10.
+ */
+public class BlockingQueue {
+    private ArrayBlockingQueue queue = new ArrayBlockingQueue(10);
+
+    public static void main(String[] args) {
+        BlockingQueue test = new BlockingQueue();
+        Producer producer = test.new Producer();
+        Consumer consumer = test.new Consumer();
+
+        Thread t1 = new Thread(producer);
+        Thread t2 = new Thread(consumer);
+
+        t1.start();
+        t2.start();
+    }
+    class Producer implements Runnable{
+
+        @Override
+        public void run() {
+            try {
+                while (true) {
+                    System.out.println("add");
+                    queue.put(1);
+
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    class Consumer implements Runnable{
+
+        @Override
+        public void run() {
+            try {
+                while (true) {
+                    System.out.println("remove");
+                    queue.take();
+
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/main/java/com/algorithm/offer/NonBlocking.java
@@ -0,0 +1,66 @@
+package com.algorithm.offer;
+
+import java.util.LinkedList;
+
+/**
+ * Created by donghao on 2018/4/10.
+ */
+public class NonBlocking {
+    private LinkedList list = new LinkedList();
+
+    public static void main(String[] args) {
+        NonBlocking noBlock = new NonBlocking();
+        Produce produce = noBlock.new Produce();
+        Consume consume = noBlock.new Consume();
+
+        Thread t1 = new Thread(produce);
+        Thread t2 = new Thread(consume);
+        t1.start();
+        t2.start();
+    }
+    class Produce implements Runnable {
+
+        @Override
+        public void run() {
+            while (true) {
+                synchronized (list) {
+                    while (list.size() >= 10) {
+                        System.out.println("满了。等待中");
+                        try {
+                            list.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    list.add(1);
+                    System.out.println("添加1个");
+                    list.notify();
+                }
+            }
+
+        }
+    }
+
+    class Consume implements Runnable {
+
+        @Override
+        public void run() {
+            while (true) {
+                synchronized (list) {
+                    while (list.size() == 0) {
+                        try {
+                            System.out.println("空了，等待中");
+                            list.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    list.remove();
+                    System.out.println("消费1个");
+                    list.notify();
+                }
+            }
+
+        }
+    }
+}
